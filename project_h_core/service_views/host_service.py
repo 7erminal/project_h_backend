6e7502bdf829444d6f1cb8cc4f1bdddf0f5957bb
Terminal-Adawe <bede.abbe91@gmail.com>
2026-03-23 18:52:35 +0000

    changes

--- project_h_core/service_views/host_service.py
+++ project_h_core/service_views/host_service.py
@@ -173,7 +173,7 @@ class HostServiceViewSet(viewsets.ViewSet):
                 service_name=service_name,
                 description=serializer.data['description'],
                 location=serializer.data['location'],
-                location_coordinates=serializer.data['location_coordinates'],
+                location_cordinates=serializer.data['location_coordinates'],
                 process=processed,
                 expected_duration=serializer.data['duration'],
                 price=price,
